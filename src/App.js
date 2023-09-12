25380c7f84005c8b092f6f5e666ac6cc2cdae6ed
waleed bin khalid <waleedbinkhalid84587@gmail.com>
2023-09-12 19:47:52 +0500

    changes done

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ function App() {
       const licenseKey = "sRwAAAYiY2FyZC1zY2FuLXZlcmNlbC1kZXBsb3kudmVyY2VsLmFwcAHp+mMmWs3YlAzquODHyQaMUPRtdrU1hYoTtxs8PkDQ6iiy8SKGtHM3+goNSYELhwDXg7SU0U/VrRwurnMey2WPrGbOqwgMsI/iA7Yh1hNVT/eoEMvcV18CE8uG0ea4D1sJ8eaDiWtC1nRjt5LDxjgLoOOEwv+bJgwnVE81TT82WHgyNsBcQJ1m";
     if (BlinkCardSDK.isBrowserSupported()) {
       const loadSettings = new BlinkCardSDK.WasmSDKLoadSettings(licenseKey);
+      loadSettings.scanBothSides = false;
 
       BlinkCardSDK.loadWasmModule(loadSettings).then(
         async (wasmSDK) => {
@@ -35,7 +36,7 @@ function App() {
             wasmSDK,
             [recognizer],
             true,
-            callbacks
+            // callbacks
           );
 
           // const cameraFeed = document.getElementsByClassName( "myCameraVideoElement" );
