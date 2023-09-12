0cdc93d544a906b5e1bdbd61e69a51cb58ed000d
waleed bin khalid <waleedbinkhalid84587@gmail.com>
2023-09-12 15:09:56 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Elements } from "@stripe/react-stripe-js";
 import PaymentComponent from './PaymentCompoenent';
 //pk_live_51N3G7KH3CgfJQbH9oOe2zba2yBt21edBEsNMaKSVtGZnv1IxzdiZ4Y8jtIpdA7ySgl4G7K9Dlufhn8awjkJAFfco00i7lpd1YF
 //pk_test_51N3G7KH3CgfJQbH9UvRGNfeXUCzOLRTIpfmUH20uAEejjEIQGSJuQNMADI25hqwGMBMoGuWhwDtRw0dpdB4nEjer00lFEVhvvI
-const stripePromise = loadStripe("pk_live_51N3G7KH3CgfJQbH9oOe2zba2yBt21edBEsNMaKSVtGZnv1IxzdiZ4Y8jtIpdA7ySgl4G7K9Dlufhn8awjkJAFfco00i7lpd1YF");
+const stripePromise = loadStripe("pk_test_51N3G7KH3CgfJQbH9UvRGNfeXUCzOLRTIpfmUH20uAEejjEIQGSJuQNMADI25hqwGMBMoGuWhwDtRw0dpdB4nEjer00lFEVhvvI");
 
 function App() {
   return (
